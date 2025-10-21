4610c9b787c4f01007674b2155a4a0084c89ff42
usrbinkat <usrbinkat@braincraft.io>
2025-10-21 10:52:41 -0700

    simplify ubuntu customization

--- images/ubuntu-24.04/env.sh
+++ images/ubuntu-24.04/env.sh
@@ -1,4 +1,4 @@
-VIRT_SYSPREP_OPERATIONS=user-account,logfiles,customize,bash-history,net-hostname,net-hwaddr,machine-id,dhcp-server-state,dhcp-client-state,yum-uuid,udev-persistent-net,tmp-files,smolt-uuid,rpm-db,package-manager-cache
+VIRT_SYSPREP_OPERATIONS=user-account,logfiles,customize,bash-history,tmp-files,smolt-uuid
 BASE_URL=https://cloud-images.ubuntu.com/noble/current
 AMD64_DOWNLOAD_FILE=noble-server-cloudimg-amd64.img
 ARM64_DOWNLOAD_FILE=noble-server-cloudimg-arm64.img
