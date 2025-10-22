8eeac48e6fdceb3a8aec9aefd2d2e5bb36b6f253
usrbinkat <usrbinkat@braincraft.io>
2025-10-22 16:31:31 -0700

    debug/simplify fedora 42 build

--- images/fedora-42/env.sh
+++ images/fedora-42/env.sh
@@ -1,6 +1,5 @@
 # VIRT_SYSPREP_OPERATIONS=user-account,logfiles,customize,bash-history,machine-id,yum-uuid,tmp-files,smolt-uuid,package-manager-cache
-# Minimal operations - only customize to avoid breaking DNS/network
-VIRT_SYSPREP_OPERATIONS=customize
+VIRT_SYSPREP_OPERATIONS=user-account,logfiles,bash-history
 _ARCH=$(echo ${ARCH} | sed 's/amd64/x86_64/;s/arm64/aarch64/')
 BASE_URL=https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/"${_ARCH}"/images
 DOWNLOAD_FILE=Fedora-Cloud-Base-Generic-42-1.1."${_ARCH}".qcow2
