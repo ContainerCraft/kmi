b0082918da01a34c60dce447531775b3a4a64166
usrbinkat <usrbinkat@braincraft.io>
2025-10-23 21:01:04 +0000

    fix(opensuse-tumbleweed): update SHA256 checksum for latest snapshot
    openSUSE Tumbleweed is a rolling release that updates frequently. Updated checksum to match the current snapshot.
    
    Old checksum: d52f74039dc0217dd0e3110812d36cfb06bc22496fd4df44c156abae1bb2beb6
    
    New checksum: 3851aeb437261243999a0f20085270351ebe108bc69ad00bacf9d21d328b4c6a
    
    This checksum will need periodic updates as Tumbleweed releases new snapshots.

--- images/opensuse-tumbleweed/env.sh
+++ images/opensuse-tumbleweed/env.sh
@@ -1,5 +1,5 @@
 VIRT_SYSPREP_OPERATIONS=logfiles,bash-history,package-manager-cache
 AMD64_BASE_URL=https://download.opensuse.org/tumbleweed/appliances/
 AMD64_DOWNLOAD_FILE=openSUSE-Tumbleweed-Minimal-VM.x86_64-kvm-and-xen.qcow2
-AMD64_SHA256SUM=d52f74039dc0217dd0e3110812d36cfb06bc22496fd4df44c156abae1bb2beb6
+AMD64_SHA256SUM=3851aeb437261243999a0f20085270351ebe108bc69ad00bacf9d21d328b4c6a
 SKIP=ssh
