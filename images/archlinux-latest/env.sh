fc95b9e0eaf2253d1a300d5d6ec5892dcfa4fd2c
usrbinkat <usrbinkat@braincraft.io>
2025-10-23 12:42:12 -0700

    disable customize on arch

--- images/archlinux-latest/env.sh
+++ images/archlinux-latest/env.sh
@@ -1,4 +1,4 @@
-VIRT_SYSPREP_OPERATIONS=logfiles,customize,bash-history,net-hostname,net-hwaddr,machine-id,dhcp-server-state,dhcp-client-state,tmp-files,smolt-uuid,package-manager-cache
+VIRT_SYSPREP_OPERATIONS=logfiles,bash-history,net-hostname,net-hwaddr,machine-id,dhcp-server-state,dhcp-client-state,tmp-files,smolt-uuid,package-manager-cache
 AMD64_BASE_URL=https://geo.mirror.pkgbuild.com/images/latest/
 AMD64_DOWNLOAD_FILE=Arch-Linux-x86_64-cloudimg.qcow2
 AMD64_SHA256SUM=a00c8d239c419811c890fdcd17e2a62c81af31e2efbe84c613bcbef51f030215
