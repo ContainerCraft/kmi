bd25b70f665c97cda5c1f19e68149f8ce2f3b4c0
usrbinkat <usrbinkat@braincraft.io>
2025-10-25 15:50:01 -0700

    fix(debian,ubuntu): use run-command for resolv.conf creation
    Change from virt-sysprep write/append-line operations to run-command
    with printf to create /etc/resolv.conf. The write operation fails when
    the file doesn't exist in the guest filesystem.
    
    Also remove machine-id operation from Debian 13 to match working
    configurations and avoid operation ordering conflicts.
    
    This fixes the error:
      virt-sysprep: error: libguestfs error: internal_write: open:
      /etc/resolv.conf: No such file or directory

--- images/debian-13/env.sh
+++ images/debian-13/env.sh
@@ -1,6 +1,7 @@
 # Preserve user-account for flexibility, enable customize for package installation
 # customize operation REQUIRED to install packages and enable services
-VIRT_SYSPREP_OPERATIONS=user-account,logfiles,bash-history,machine-id,tmp-files,customize
+# machine-id removed to avoid conflicts with resolv.conf creation
+VIRT_SYSPREP_OPERATIONS=user-account,logfiles,bash-history,tmp-files,customize
 # Using cdimage.debian.org instead of cloud.debian.org subdomain for better CircleCI connectivity
 BASE_URL=https://cdimage.debian.org/images/cloud/trixie/latest/
 DOWNLOAD_FILE=debian-13-generic-${ARCH}.qcow2
